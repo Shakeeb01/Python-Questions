1fa1fbc792742d78afacf576dfd10be75f017521
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-08-13 00:37:06 -0700

    Day 1 of Building Logic in python

---
+++ Day_1/FirstQuestion.py
@@ -0,0 +1,4 @@
+# Write a Python program to print numbers from 1 to 100.
+
+for i in range(1,101):
+    print(i)
---
+++ Day_1/SecondQuestion.py
@@ -0,0 +1,5 @@
+#  print all the even numbers from 1 to 50.
+
+for i in range(1,51):
+    if i % 2 == 0:
+        print(i)
