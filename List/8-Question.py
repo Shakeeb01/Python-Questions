06860458ba6a4cd9e860d604e95fbcd340cebc8b
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-08-27 09:32:15 +0500

    modify the 8th question.

--- List/8-Question.py
+++ List/8-Question.py
@@ -2,9 +2,19 @@
 
 s = ["s","h","a","k","i","b"]
 
-new_str = []
+left, right = 0, len(s)-1
 
-for i in range(len(s)-1,-1,-1):
-    new_str.append(s[i])
+
+while left < right:
+    # Swap characters at positions left and right
+    s[left], s[right] = s[right], s[left]
+    # Move the pointers towards the center
     
-print(new_str)    
+    left += 1
+    right -= 1
+    
+print(s)    
+
+
+    
+   
